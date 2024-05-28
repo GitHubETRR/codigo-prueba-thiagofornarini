0374ba8f2c0f469a99fd4631530915dd36230a67
thiagofornarini <tfornarini@etrr.edu.ar>
2024-05-28 15:55:38 -0300

    Finalizacion codigo triangulos

--- Documentos/Codigos/angulosTriangulo.c
+++ Documentos/Codigos/angulosTriangulo.c
@@ -7,9 +7,13 @@ int main(void){
     printf ("Ingrese los valores de los angulos conocidos: \n");
     scanf ("%f %f", &angulo1, &angulo2);
     float angulo3= 180-angulo1-angulo2;
-    if (180>angulo3 && angulo3>0 ){
-        printf ("Su angulo es %f", angulo3 );
+    if (0>angulo1 || 0>angulo2){
+    printf("Ingrese angulos positivos porfavor");
     }else{
-        printf("Ingrese valores de angulos reales");
+        if (180>angulo3 && angulo3>0 ){
+            printf ("Su angulo es %f", angulo3 );
+        }else{
+            printf("Ingrese valores de angulos menores a 180");
         }
+    }
 }
